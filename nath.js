f4be8a3b2ec62cb1e8e1e5a1129939f6e9737043
oldnathan <43586509+oldnathan@users.noreply.github.com>
2018-09-26 19:35:05 +0300

    Update nath.js

--- nath.js
+++ nath.js
@@ -3,30 +3,13 @@ const client = new Discord.Client();
 
 console.log("Welcome");
 
-var PrEfix = "$$";
-client.on('message', message => {
-  if (!message.content.startsWith(PrEfix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '445630664671232000') return;
-if (message.content.startsWith(PrEfix + 'wt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
- if (message.author.id !== '445630664671232000') return;
-if (message.content.startsWith(PrEfix + 'pl')) {
-client.user.setActivity(argresult, {type:'PLAYING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'ls')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-}
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  client.user.setActivity('Music and some rest', { type: 'Playing' });
+  client.user.setStatus("dnd");
 });
+
 
 client.login(process.env.BOT_TOKEN);
