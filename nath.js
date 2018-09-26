30ed4047125a4b2b263128c7146bc60c3be91c96
oldnathan <43586509+oldnathan@users.noreply.github.com>
2018-09-26 20:00:49 +0300

    Add files via upload

---
+++ nath.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+console.log("Welcome");
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+ 
+});
+ 
+client.on('guildMemberAdd',async member => {
+  if(member.guild.id !== '462347265747451914') return;
+  setTimeout(function(){
+  member.guild.channels.find(r => r.id === '492406462476451840').send('Welcome To **Bõubbles** Tumblr .');
+},3000);
+});
+
+client.login(process.env.BOT_TOKEN);
